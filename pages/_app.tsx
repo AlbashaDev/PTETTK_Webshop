99c1e64fbaa20cf6da965da9d77adc0757e7e42d
Albasha99 <albashahmad.hu@gmail.com>
2023-05-25 21:56:45 +0200

    added redux-thunk redux-persist to save cart state locally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import { persistor, store } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     // Higher order component
     <SessionProvider session={session}>
       <Provider store={store}>
-        <Toaster />
-        <Component {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Toaster />
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </SessionProvider>
   );
--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "./basketSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+key: "root",
+ storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, basketReducer);
 
 export const store = configureStore({
-  reducer: {
-    basket: basketReducer,
-  },
+
+ reducer: {
+basket: persistedReducer,
+ },
+ middleware: [thunk],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
